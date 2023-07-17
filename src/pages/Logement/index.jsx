0fae37e4431404a458caf4cb6e18d95eea629c1f
Tassadit Idiri <mamantasse@gmail.com>
2023-07-17 16:23:05 +0200

    dernieres retouche css

--- src/pages/Logement/index.jsx
+++ src/pages/Logement/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { useParams} from "react-router-dom";
+import { useParams } from "react-router-dom";
 import LogementData from "../../Data/logements.json";
 import ErrorPage from "../ErrorPage";
 import Carrousel from "../../components/carrousel";
@@ -10,14 +10,11 @@ import Host from "../../components/Host";
 import Collapse from "../../components/Collapse";
 import Tag from "../../components/Tag";
 
-
 const Logement = () => {
   const id = useParams().id;
   console.log(id);
   const logement = LogementData.find((logement) => logement.id === id);
-  
 
-  
   /* Tags */
   const tagsLogement = logement?.tags.map((tags, i) => {
     return <Tag key={i} nom={tags} />;
@@ -36,57 +33,38 @@ const Logement = () => {
     return <ErrorPage />;
   }
 
- 
   return (
-  
-        <div className="Fiche-container">
-          <Carrousel pictures={logement.pictures} />
-          <section className="Fiche-logement">
-            <div className="description-info">
-              <div className="description-info__titletags">
-                <div className="description-info__titletags__title">
-                  <span className="titre-logement">{logement?.title}</span>
-                  <span className="endroit-logement">
-                    {logement?.location}
-                  </span>
-                </div>
-              {/* Tags */}
-              <div className="description-info__titletags__tags">
-                  {tagsLogement}
-                </div>
-              </div>
-
-              <div className="description-info__proprietaire">
-                {/* Hosting */}
-                <div className="description-info__proprietaire__nom-prop">
-                  <Host
-                    name={logement?.host.name}
-                    picture={logement?.host.picture}
-                  />
-                </div>
-                {/* Rating */}
-                <div className="description-info__proprietaire__rate">
-                  <Rating score={logement.rating} />
-                </div>
-              </div>
-            </div>
-          </section>
-          {/* Collapse */}
-          <div className="description-centent">
-            <div className="description-centent__description">
-              <Collapse
-                title="Description"
-                content={logement?.description}
-              />
-            </div>
-            <div className="description-centent__equipement">
-              <Collapse title="Équipements" content={equipements} />
-            </div>
-          </div>
+    <div className="Fiche-container">
+      <Carrousel pictures={logement.pictures} />
+      <div className="titreEtProprietaire">
+        <div className="titreEtProprietaire__info__title">
+          <span className="titre-logement">{logement?.title}</span>
+          <span className="endroit-logement">{logement?.location}</span>
+          {/* Tags */}
+          <div className="titreEtProprietaire__info__tags">{tagsLogement}</div>
         </div>
-    )
-
-  };
-
+        <div className="titreEtProprietaire__propietaire">
+        {/* Hosting */}
+        <div className="description-info__proprietaire__nom-prop">
+          <Host name={logement?.host.name} picture={logement?.host.picture} />
+        </div>
+        {/* Rating */}
+        <div className="description-info__proprietaire__rate">
+          <Rating score={logement.rating} />
+        </div>
+      </div>
+      </div>
+      
+      <div className="description-centent">
+        <div className="description-centent__description">
+          <Collapse title="Description" content={logement?.description} />
+        </div>
+        <div className="description-centent__equipement">
+          <Collapse title="Équipements" content={equipements} />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Logement;
