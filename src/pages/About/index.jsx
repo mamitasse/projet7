c72ab066eaec8fc17e6021c9b4258098ee2a4afd
Tassadit Idiri <mamantasse@gmail.com>
2023-07-20 01:16:41 +0200

    correction apres soutenance

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -6,8 +6,8 @@ import "./About.css";
 const About = () => {
   return (
     <main className="main_about">
-      <article className="banner">
-        <img className="banner_img" src={BannerAbout} alt="paysage montagne" />
+      <article className="banner_apropos">
+        <img className="banner_apropos_img" src={BannerAbout} alt="paysage montagne" />
       </article>
       <LayoutAbout />
     </main>
